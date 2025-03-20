8b919ff42f8581c0594f83b5132c94e453f4a3d2
Tom Cat <48447545+tx-tomcat@users.noreply.github.com>
2025-03-21 00:52:19 +0700

    add create oft adapter impl

--- components/common/SkeletonCard.tsx
+++ components/common/SkeletonCard.tsx
@@ -3,10 +3,10 @@ import { Skeleton } from "@/components/ui/skeleton";
 export function SkeletonCard() {
   return (
     <div className="flex flex-col w-full justify-center items-center gap-2">
-      <Skeleton className="h-[125px] w-[90%] rounded-xl" />
+      <Skeleton className="h-[125px] w-full rounded-xl" />
       <div className="w-full flex flex-col gap-2 items-center">
-        <Skeleton className="h-4 w-[90%]" />
-        <Skeleton className="h-4 w-[90%]" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
       </div>
     </div>
   );
