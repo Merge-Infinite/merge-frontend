4973aef87b53edf9279dde0edc0f36cdf3aaf079
Tom Cat <48447545+tx-tomcat@users.noreply.github.com>
2025-04-02 17:39:06 +0700

    Update system architecture documentation with service interactions

--- components/common/user-info.tsx
+++ components/common/user-info.tsx
@@ -6,6 +6,7 @@ import { TabMode, updateTabMode } from "@/lib/wallet/store/app-context";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
+import { Button } from "../ui/button";
 interface GamePlayInfoProps {
   explore?: number;
   reward?: number;
@@ -18,12 +19,43 @@ export default function UserInfo({}: GamePlayInfoProps) {
   const router = useRouter();
   const { user } = useUser();
   const dispatch = useDispatch<AppDispatch>();
+  const clearData = () => {
+    // Clear localStorage
+    localStorage.clear();
+
+    // Clear IndexedDB
+    const databases = indexedDB.databases
+      ? indexedDB.databases()
+      : Promise.resolve([]);
+    databases
+      .then((dbs) => {
+        dbs.forEach((db) => {
+          if (db.name) {
+            indexedDB.deleteDatabase(db.name);
+          }
+        });
+
+        // Reload the page to apply changes
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error("Error clearing IndexedDB:", error);
+      });
+  };
+
   return (
     <div className="w-full px-4 py-2 bg-neutral-950/60 rounded-2xl border border-[#1f1f1f] flex-col justify-start items-start gap-2 inline-flex">
       <div className="self-stretch justify-center items-center gap-2 inline-flex">
         <div className="grow shrink basis-0 text-white text-base font-bold font-['Sora'] leading-normal capitalize">
           Hi {user?.username}!!!
         </div>
+        <Button
+          className="w-fit px-2 py-1 rounded-full"
+          size="sm"
+          onClick={clearData}
+        >
+          Clear data
+        </Button>
       </div>
       <div className="self-stretch justify-start items-center gap-6 inline-flex">
         <div
