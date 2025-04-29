267cc59f942241ef1205429af6bf75db4f162be9
Tom Cat <48447545+tx-tomcat@users.noreply.github.com>
2025-04-29 21:24:16 +0700

    implement ONFT

--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -3,6 +3,7 @@
 import { initBackButton } from "@telegram-apps/sdk";
 
 import ElementItem from "@/components/common/ElementItem";
+import { SkeletonCard } from "@/components/common/SkeletonCard";
 import { RecipeDetail } from "@/components/screen/play/recipe-detail";
 import { Button } from "@/components/ui/button";
 import {
@@ -14,10 +15,9 @@ import {
 import { useAdsgram } from "@/hooks/useAdsgram";
 import useApi from "@/hooks/useApi";
 import { useUser } from "@/hooks/useUser";
-import { shortenName } from "@/lib/utils";
 import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useMemo, useState } from "react";
+import { Fragment, useEffect, useMemo, useState } from "react";
 import { toast } from "sonner";
 
 export default function SubmitItem() {
@@ -78,106 +78,121 @@ export default function SubmitItem() {
     );
   }, [user?.userBalance?.subscriptionEndDate]);
 
-  return (
-    <div className="flex flex-col h-full gap-4 p-4">
-      <div className="self-stretch justify-start items-center gap-2 inline-flex">
-        <div className="px-3 py-1 rounded-3xl border border-white justify-center items-center gap-2 flex">
-          {fetchItemChallenge?.data?.item?.emoji}
-          <div className="text-white text-xs  leading-normal">
-            {shortenName(fetchItemChallenge?.data?.item?.handle)}
-          </div>
-        </div>
-        <Button
-          isLoading={isLoading}
-          disabled={isLoading}
-          onClick={() => {
-            if (!isEligible) {
-              showAd();
-            } else {
-              setIsRecipeDetailOpen(true);
-            }
-          }}
-          className="px-3 py-1 rounded-3xl border border-white justify-center items-center gap-2 flex w-fit"
-        >
-          <div className="text-white text-xs font-normal leading-normal">
-            View Recipe
-          </div>
-          <Image
-            src="/images/ad.svg"
-            alt="arrow-right"
-            width={24}
-            height={24}
-          />
-        </Button>
-        <div className="justify-start items-start flex">
-          <Image src={"images/points.svg"} alt="item" width={16} height={16} />
-          <div className="text-center text-white text-sm font-normal font-['Sora'] leading-normal">
-            +1 Point
-          </div>
-        </div>
-      </div>
-      <div
-        className={`flex px-3 py-1 rounded-3xl border border-[#333333] justify-center items-center gap-2 w-fit ${
-          fetchCraftedItemsToday?.data
-            ? "bg-white text-[#333]"
-            : "bg-[#333333] text-white"
-        }`}
-      >
-        {fetchItemChallenge?.data?.item?.emoji}
-        <div className="text-xs font-normal leading-normal">
-          {fetchItemChallenge?.data?.item?.handle}
-        </div>
-      </div>
-      <Button
-        className="px-3 py-1  rounded-3xl justify-center items-center gap-2 inline-flex w-fit"
-        disabled={
-          !fetchCraftedItemsToday?.data || submitItemChallenge?.isPending
-        }
-        onClick={() => {
-          submitItemChallenge?.mutateAsync({
-            itemChallengeId: itemChallengeId,
-          });
-        }}
-        isLoading={submitItemChallenge?.isPending}
-      >
-        <div className="text-[#333333] text-xs font-normal font-['Sora'] uppercase leading-normal">
-          Submit
-        </div>
-      </Button>
-      <div className="text-white text-sm font-normal font-['Sora'] leading-normal">
-        If you don't have this element available:
-      </div>
-      <Button
-        className="px-3 py-1  rounded-3xl justify-center items-center gap-2 inline-flex w-fit"
-        onClick={() => {
-          router.push("/play");
-        }}
-      >
-        <div className="text-center text-neutral-950 text-xs font-normal font-['Sora'] uppercase leading-normal">
-          Merge
-        </div>
-      </Button>
+  console.log(fetchItemChallenge?.isFetching);
 
-      <Sheet open={isRecipeDetailOpen} onOpenChange={setIsRecipeDetailOpen}>
-        <SheetContent
-          side="bottom"
-          className="bg-[#141414] text-white border-t border-[#333333]"
-          showClose={true}
-          style={{
-            height: "90%",
-          }}
-        >
-          <SheetHeader className="w-[80%] flex flex-row items-center gap-2">
-            <SheetTitle className="text-white text-center">
-              <ElementItem
-                {...fetchItemChallenge?.data?.item}
-                amount={undefined}
+  return (
+    <div className="flex flex-col h-full gap-4 p-4 w-full">
+      {fetchItemChallenge?.isFetching ? (
+        <div className="flex justify-center items-center h-full">
+          <SkeletonCard />
+        </div>
+      ) : (
+        <Fragment>
+          <div className="grid grid-cols-3 gap-2">
+            <div className="px-2 py-1 rounded-3xl border border-white justify-center items-center gap-2 flex col-start-1">
+              {fetchItemChallenge?.data?.item?.emoji}
+              <div className="text-white text-xs  leading-normal truncate">
+                {fetchItemChallenge?.data?.item?.handle}
+              </div>
+            </div>
+            <Button
+              isLoading={isLoading}
+              disabled={isLoading}
+              onClick={() => {
+                if (!isEligible) {
+                  showAd();
+                } else {
+                  setIsRecipeDetailOpen(true);
+                }
+              }}
+              className="px-3 py-1 rounded-3xl border border-white justify-center items-center gap-2 flex w-fit col-start-2"
+            >
+              <div className="text-white text-xs font-normal leading-normal">
+                View Recipe
+              </div>
+              <Image
+                src="/images/ad.svg"
+                alt="arrow-right"
+                width={24}
+                height={24}
               />
-            </SheetTitle>
-          </SheetHeader>
-          <RecipeDetail item={fetchItemChallenge?.data?.item} />
-        </SheetContent>
-      </Sheet>
+            </Button>
+            <div className="flex justify-start items-center flex-row col-start-3">
+              <Image
+                src={"images/points.svg"}
+                alt="item"
+                width={16}
+                height={16}
+              />
+              <div className="text-center text-white text-sm font-normal font-['Sora'] leading-normal">
+                +1 Point
+              </div>
+            </div>
+          </div>
+          <div
+            className={`flex px-3 py-1 rounded-3xl border border-[#333333] justify-center items-center gap-2 w-fit ${
+              fetchCraftedItemsToday?.data
+                ? "bg-white text-[#333]"
+                : "bg-[#333333] text-white"
+            }`}
+          >
+            {fetchItemChallenge?.data?.item?.emoji}
+            <div className="text-xs font-normal leading-normal">
+              {fetchItemChallenge?.data?.item?.handle}
+            </div>
+          </div>
+          <Button
+            className="px-3 py-1  rounded-3xl justify-center items-center gap-2 inline-flex w-fit"
+            disabled={
+              !fetchCraftedItemsToday?.data || submitItemChallenge?.isPending
+            }
+            onClick={() => {
+              submitItemChallenge?.mutateAsync({
+                itemChallengeId: itemChallengeId,
+              });
+            }}
+            isLoading={submitItemChallenge?.isPending}
+          >
+            <div className="text-[#333333] text-xs font-normal font-['Sora'] uppercase leading-normal">
+              Submit
+            </div>
+          </Button>
+          <div className="text-white text-sm font-normal font-['Sora'] leading-normal">
+            If you don't have this element available:
+          </div>
+          <Button
+            className="px-3 py-1  rounded-3xl justify-center items-center gap-2 inline-flex w-fit"
+            onClick={() => {
+              router.push("/play");
+            }}
+          >
+            <div className="text-center text-neutral-950 text-xs font-normal font-['Sora'] uppercase leading-normal">
+              Merge
+            </div>
+          </Button>
+
+          <Sheet open={isRecipeDetailOpen} onOpenChange={setIsRecipeDetailOpen}>
+            <SheetContent
+              side="bottom"
+              className="bg-[#141414] text-white border-t border-[#333333]"
+              showClose={true}
+              style={{
+                height: "90%",
+              }}
+            >
+              <SheetHeader className="w-[80%] flex flex-row items-center gap-2">
+                <SheetTitle className="text-white text-center">
+                  <ElementItem
+                    {...fetchItemChallenge?.data?.item}
+                    amount={undefined}
+                  />
+                </SheetTitle>
+              </SheetHeader>
+              <RecipeDetail item={fetchItemChallenge?.data?.item} />
+            </SheetContent>
+          </Sheet>
+        </Fragment>
+      )}
     </div>
   );
 }
--- components/common/SkeletonCard.tsx
+++ components/common/SkeletonCard.tsx
@@ -5,7 +5,7 @@ interface SkeletonCardProps {}
 export function SkeletonCard({}: SkeletonCardProps) {
   // Show loading skeleton
   return (
-    <div className="flex flex-col w-full max-w-sm mx-auto bg-black rounded-2xl p-6 shadow-sm overflow-hidden relative h-full">
+    <div className="flex flex-col w-full max-w-sm mx-auto bg-black rounded-2xl py-6 shadow-sm overflow-hidden relative h-full">
       <div className="mt-0 space-y-4">
         <Skeleton className="h-4 w-3/4 bg-gray-200 dark:bg-gray-700 rounded" />
         <Skeleton className="h-4 w-1/2 bg-gray-200 dark:bg-gray-700 rounded" />
