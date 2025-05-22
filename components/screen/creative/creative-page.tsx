6ebfaf8edbaf0f25b683ead5e7cc18f3e0ce6da8
Tom Cat <48447545+tx-tomcat@users.noreply.github.com>
2025-05-23 03:31:54 +0700

    multiple dataouces

--- components/screen/creative/creative-page.tsx
+++ components/screen/creative/creative-page.tsx
@@ -612,11 +612,11 @@ const CreatureCustomizer = () => {
               <Button
                 className="self-stretch px-4 py-2 bg-[#a668ff] rounded-3xl inline-flex justify-center items-center gap-2"
                 onClick={handleMintClick}
-                disabled={isLoading}
-                isLoading={isLoading}
+                disabled={isLoading || isPending}
+                isLoading={isLoading || isPending}
               >
                 <div className="justify-start text-neutral-950 text-sm font-normal uppercase leading-normal">
-                  {isLoading ? "Minting..." : "Mint"}
+                  {isLoading || isPending ? "Minting..." : "Mint"}
                 </div>
               </Button>
             </div>
