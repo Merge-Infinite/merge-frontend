6a1bbd90b02405d9de3f8cc3e0bf84ce41d4231c
Tom Cat <48447545+tx-tomcat@users.noreply.github.com>
2025-05-23 21:48:48 +0700

    docker fix

--- components/screen/home/game.tsx
+++ components/screen/home/game.tsx
@@ -2,14 +2,12 @@
 
 import UserInfo from "../../common/user-info";
 import ChallengeGrid from "./ChallengeGrid";
-import ChallengeHeader from "./ChallengeHeader";
 
 export function GameScreen() {
   return (
     <div className="w-full h-full  flex-col justify-start items-start gap-2 inline-flex">
       <UserInfo />
       <div className="flex-col justify-start items-start gap-4 inline-flex">
-        <ChallengeHeader />
         <ChallengeGrid category="Trend" />
       </div>
     </div>
