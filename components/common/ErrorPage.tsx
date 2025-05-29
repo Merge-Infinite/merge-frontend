d17f9e3470a4b1b87cd915659d00f7011eedae77
Tom Cat <48447545+tx-tomcat@users.noreply.github.com>
2025-05-29 15:42:34 +0700

    fixing build file

--- components/common/ErrorPage.tsx
+++ components/common/ErrorPage.tsx
@@ -14,12 +14,12 @@ export function ErrorPage({
   return (
     <div className="flex flex-col flex-1 h-full w-full justify-center items-center gap-10 relative ">
       <div className=" text-white text-center flex items-center justify-center text-[3rem] font-light  uppercase relative">
-        The Merges Infinite
+        Something went wrong.
       </div>
 
       <Button
         onClick={() => {
-          window.open("https://merg3.xyz", "_blank");
+          window.open("https://app.merg3.xyz", "_blank");
         }}
         className="w-fit px-4 capitalize"
       >
