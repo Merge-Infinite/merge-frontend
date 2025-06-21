16285399cf94ac65a3644e5c194362bc40bf54e2
Tom Cat <alexthepump89@gmail.com>
2025-06-21 18:31:59 +0700

    close tab when unscribe

--- app/nft/page.tsx
+++ app/nft/page.tsx
@@ -338,8 +338,6 @@ const NFTCard = ({
   availableSlots: number;
   nftCount: number | undefined;
 }) => {
-  alert(nftCount);
-  alert(availableSlots);
   return (
     <div className="w-44 flex flex-col items-center gap-2">
       <Card className="w-44 h-44 bg-neutral-800 border-0 rounded-2xl overflow-hidden">
@@ -368,7 +366,10 @@ const NFTCard = ({
         className="w-44 h-6 px-4 bg-white text-black hover:bg-gray-200 rounded-3xl text-xs font-normal font-sora uppercase"
         onClick={() => handleStakeNFT(item.id)}
         disabled={
-          isLoading || (nftCount !== undefined && availableSlots <= nftCount)
+          isLoading ||
+          (nftCount !== undefined && availableSlots <= nftCount) ||
+          nftCount === undefined ||
+          availableSlots == 0
         }
         isLoading={isLoading}
       >
